8433dfea6f3ebcfcad52191ab06147175fe951a6
XoXien <xo.jbmergenio.game@gmail.com>
2024-12-12 20:27:46 +0800

    Temperature

---
+++ Activity/Activity4.py
@@ -0,0 +1,21 @@
+#Celsius to Farenheit
+#F=Fahrenheit C=Celsius
+F = eval(input("Enter Temperature in FARENHEIT: "))
+
+#Formula 
+C = ((F - 32)*5)/9
+round(C,2) # To limit the decimal into two (2) places
+
+#Output
+print(f"{F} degrees in Farenheit converted to Celsius is {C}")
+
+#Farenheit to Celsius
+C = eval(input("Enter Temperature in CELSIUS: "))
+
+#Formula
+F = (C * 9/5) + 32
+
+#Output
+#'Round' can be place directly inside the print 
+print(f"{C} degrees in Celsius converted to Farenheit is {round(F,2)}")
+
