5389308d7a6650a28ed93dac517b4a24f5a7fb64
XoXien <xo.jbmergenio.game@gmail.com>
2024-12-12 21:05:06 +0800

    Assignment Operators

---
+++ Activity/Activity5.2.py
@@ -0,0 +1,33 @@
+#Assignment Operators
+#Used to assign values to variables
+
+#Addition (+=)
+a = 5
+a += 1
+print(a)
+a += 1
+print(a)
+
+print()
+#Subtraction (-=)
+s = 5
+s -= 1
+print(s)
+s -= 1
+print(s)
+
+print()
+#Multiplication (*=)
+m = 5
+m *= 2
+print(m)
+m *= 2
+print(m)
+
+print()
+#Division (/=)
+d = 20
+d /= 2
+print(d)
+d /= 2
+print(d)
