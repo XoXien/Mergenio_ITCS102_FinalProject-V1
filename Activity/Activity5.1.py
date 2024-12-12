5d3e5352a1710c5ed8014cf337c924a58473e99e
XoXien <xo.jbmergenio.game@gmail.com>
2024-12-12 21:04:36 +0800

    Arithmetic Operators

---
+++ Activity/Activity5.1.py
@@ -0,0 +1,49 @@
+#Arithmetic Operators
+#Addition (+)
+x=10  
+y=10
+z = x + y
+#Output
+print(z)
+
+#Subtraction (-)
+z = x - y
+#Output
+print(z)
+
+#Multiplication (*)
+z = x * y
+#Output
+print(z)
+
+#Division (/)
+z = x / y
+#Output
+print(z)
+
+#Modulus
+#Remainder (Ex. 6.21 = 21)
+z = x % y
+#Output
+print(z)
+
+#Exponentiation (**)
+#Exponent
+z = x ** y
+#Output
+print(z)
+
+# Floor division (//)
+# Integer or Base Number (Ex. 6.21 = 6) 
+z = x // y
+#Output
+print(z)
+
+#Assignment Operators
+#Used to assign values to variables
+
+#Addition (+)
+a=5
+a += 1
+a += 1
+print(a)
