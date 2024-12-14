c037590996f50e3d2e1097fb755424b75cadfb3c
XoXien <xo.jbmergenio.game@gmail.com>
2024-12-14 14:03:13 +0800

    For Loop

--- Activity/Activity8.py
+++ Activity/Activity8.py
@@ -1,5 +1,7 @@
-#  A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 # Its format is "For x in range(start, stop, step)
+# This code is all about odd and even. 
+# The user will input 10 random number and the Condition inside the For Loop will Filter  it and sum all number, the odds and the evens
 
 sum = 0
 odd = 0 
@@ -12,7 +14,7 @@ for x in range(1,11):
         even += num
     else:
         odd += num
-        
+
 print(f"The sum of all numbers are: {sum}")
 print(f"The sum of all even numbers are: {even}")
 print(f"The sum of all odd numbers are: {odd}")
