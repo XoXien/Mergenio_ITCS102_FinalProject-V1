d9dabbf95da456700f7fab2580c00cc9be4c93c1
XoXien <xo.jbmergenio.game@gmail.com>
2024-12-14 23:29:42 +0800

    Diamond Print with User Input

---
+++ Code Challenge/Code_Challenge2.py
@@ -0,0 +1,4 @@
+# Diamond Print with User Input
+
+name = input("What is your name?")
+print("\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t*\t*\t* \n\t\t\t\t\t\t\t\t*\t*\t*\t*\t* \n\t\t\t\t\t\t\t*\t\t HI! "+name+"\t\t\t* \n\t\t\t\t\t\t\t\t*\t*\t*\t*\t* \n\t\t\t\t\t\t\t\t\t*\t*\t* \n\t\t\t\t\t\t\t\t\t\t*")
