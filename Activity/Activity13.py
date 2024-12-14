cf9bb14218b274e7290e37026bd11d70a15fbc43
XoXien <xo.jbmergenio.game@gmail.com>
2024-12-14 14:32:38 +0800

    Multiplication Table using For Loop

---
+++ Activity/Activity13.py
@@ -0,0 +1,7 @@
+# Multiplication Table using For Loop
+
+num = eval(input("Enter the column number:  "))
+for x in range(1,11):
+    for y in range(1,num+1):
+        print(f"{x}*{y}={x*y}",end="\t")
+    print()
